8d12fe80849a61cdabedc862cbd248ead2ee69a4
Jayden Grubb <jaydengrubb1@gmail.com>
2022-07-15 02:02:15 +1000

    Imported CAD models and added color for clarity

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace Robot
+{
+	public class GameManager : MonoBehaviour
+    {
+        private static GameManager instance;
+
+        public void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else
+            {
+                Destroy(this);
+            }
+        }
+
+        public static void Quit()
+        {
+            Application.Quit();
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        }
+    }
+}
